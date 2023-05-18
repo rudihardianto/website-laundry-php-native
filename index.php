c21c97eeadbd3a99516e2e3111c551122f5b4047
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2023-05-18 16:20:54 +0700

    halaman utama

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 <body>
     <!-- START: navbar -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
         <div class="container">
             <!-- logo -->
             <a class="navbar-brand" href="#">
@@ -39,7 +39,7 @@
                     </li>
                 </ul>
                 <!-- dropdown -->
-                <div class="dropdown me-5">
+                <div class="dropdown">
                     <a class="dropdown-toggle text-decoration-none" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         <span class="text-black">Admin</span>
@@ -55,13 +55,300 @@
     <!-- END: navbar -->
 
 
-    <!-- START: content -->
-    <div id="main" class="main-content">
+    <!-- START: welcome -->
+    <section id="welcome" class="main-welcome my-3">
         <div class="container">
-            
+            <div class="row d-flex justify-content-between align-items-center">
+                <div class="col">
+                    <div>Selamat datang, <span class="fw-bold">Admin</span></div>
+                    <h2>Dashboard</h2>
+                </div>
+                <div class="col text-end">
+                    <button type="submit" class="btn btn-primary">+ Tambah Pesanan</button>
+                </div>
+            </div>
         </div>
-    </div>
-    <!-- END: content -->
+    </section>
+    <!-- END: welcome -->
+
+    <!-- START: menu -->
+    <section id="menu" class="main-menu my-3">
+        <div class="container">
+            <div class="row">
+                <!-- karyawan -->
+                <div class="col-md-4 my-2 my-md-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row d-flex justify-content-center align-items-center">
+                                <div class="col">
+                                    <p>Jumlah Karyawan</p>
+                                    <h5 class="card-title fw-bold">10</h5>
+                                </div>
+                                <div class="col text-end">
+                                    <img src="_assets/img/team.png" alt="" width="70">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- total order -->
+                <div class="col-md-4 my-2 my-md-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row d-flex justify-content-center align-items-center">
+                                <div class="col">
+                                    <p>Total Order</p>
+                                    <h5 class="card-title fw-bold">10</h5>
+                                </div>
+                                <div class="col text-end">
+                                    <img src="_assets/img/total_order.png" alt="" width="70">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- jumlah paket -->
+                <div class="col-md-4 my-2 my-md-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row d-flex justify-content-center align-items-center">
+                                <div class="col">
+                                    <p>Jumlah Paket Tersedia</p>
+                                    <h5 class="card-title fw-bold">10</h5>
+                                </div>
+                                <div class="col text-end">
+                                    <img src="_assets/img/jumlah_paket.png" alt="" width="70">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END: menu -->
+
+    <!-- START: order cuci komplit -->
+    <section id="order-cuci-komplit" class="main-order-cuci-komplit my-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Order Cuci Komplit</h5>
+                            <hr>
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">No Order</th>
+                                            <th scope="col">TGL Order</th>
+                                            <th scope="col">Nama Pelanggan</th>
+                                            <th scope="col">Jenis Paket</th>
+                                            <th scope="col">Waktu Kerja</th>
+                                            <th scope="col">Berat (KG)</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry the Bird</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END: order cuci komplit -->
+
+    <!-- START: order cuci komplit -->
+    <section id="order-dry-clean" class="main-order-dry-clean my-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Order Dry Clean (Cuci Kering)</h5>
+                            <hr>
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">No Order</th>
+                                            <th scope="col">TGL Order</th>
+                                            <th scope="col">Nama Pelanggan</th>
+                                            <th scope="col">Jenis Paket</th>
+                                            <th scope="col">Waktu Kerja</th>
+                                            <th scope="col">Berat (KG)</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>@fat</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry the Bird</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>@twitter</td>
+                                            <td>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                                </a>
+                                                <a href="">
+                                                    <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END: order cuci komplit -->
+
+    <!-- START: order satuan -->
+    <section id="order-satuan" class="main-order-satuan my-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Order Satuan</h5>
+                            <hr>
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">No Order</th>
+                                            <th scope="col">TGL Order</th>
+                                            <th scope="col">Nama Pelanggan</th>
+                                            <th scope="col">Jenis Paket</th>
+                                            <th scope="col">Waktu Kerja</th>
+                                            <th scope="col">Berat (KG)</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td colspan="8" class="text-center">Data Tidak Tersedia</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END: order satuan -->
+
+    <!-- START: footer -->
+    <footer class="border-top p34">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col">
+                    <p class="py-3 text-muted">Copyright © 2023 | Project Skripsi Rizal hanafi</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- END: footer -->
 
 
     <script src="_assets/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
