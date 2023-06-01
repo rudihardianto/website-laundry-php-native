ffa38dfbc9aad99b1feb830297a671dd4991f5fc
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2023-06-01 08:53:50 +0700

    pisah daftar order

---
+++ daftar_order/daf_or_ck.php
@@ -0,0 +1,85 @@
+<!-- START: order cuci komplit -->
+<section id="order-cuci-komplit" class="main-order-cuci-komplit my-3">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Order Cuci Komplit</h5>
+                        <hr>
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr class="table-primary">
+                                        <th scope="col">#</th>
+                                        <th scope="col">No Order</th>
+                                        <th scope="col">Tanggal Order</th>
+                                        <th scope="col">Nama Pelanggan</th>
+                                        <th scope="col">Jenis Paket</th>
+                                        <th scope="col">Waktu Kerja</th>
+                                        <th scope="col">Berat (KG)</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        <td>@mdo</td>
+                                        <td>@mdo</td>
+                                        <td>@mdo</td>
+                                        <td>
+                                            <a href="<?=url('detail_order/detail_ck/detail_order_ck.php');?>">
+                                                <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                            </a>
+                                            <a href="">
+                                                <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                        <td>@fat</td>
+                                        <td>@fat</td>
+                                        <td>@fat</td>
+                                        <td>
+                                            <a href="<?=url('detail_order/detail_ck/detail_order_ck.php');?>">
+                                                <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                            </a>
+                                            <a href="">
+                                                <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>@twitter</td>
+                                        <td>
+                                            <a href="<?=url('detail_order/detail_ck/detail_order_ck.php');?>">
+                                                <button type="button" class="btn btn-primary btn-sm">Detail</button>
+                                            </a>
+                                            <a href="">
+                                                <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END: order cuci komplit -->
---
+++ daftar_order/daf_or_cs.php
@@ -0,0 +1,37 @@
+<!-- START: order cuci satuan -->
+<section id="order-satuan" class="main-order-satuan my-3">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Order Cuci Satuan</h5>
+                        <hr>
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr class="table-primary">
+                                        <th scope="col">#</th>
+                                        <th scope="col">No Order</th>
+                                        <th scope="col">Tanggal Order</th>
+                                        <th scope="col">Nama Pelanggan</th>
+                                        <th scope="col">Jenis Paket</th>
+                                        <th scope="col">Waktu Kerja</th>
+                                        <th scope="col">Berat (KG)</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="8" class="text-center">Data Tidak Tersedia</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END: order cuci satuan -->
