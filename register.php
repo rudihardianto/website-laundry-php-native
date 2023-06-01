00f4be0543fbfe7c55ce8610fe706e66c49ced3f
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2023-06-01 17:57:59 +0700

    ubah login, register, logout

--- register.php
+++ register.php
@@ -17,6 +17,11 @@
                                     required>
                             </div>
                             <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" name="email" class="form-control" id="email" autocomplete="off"
+                                    required>
+                            </div>
+                            <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" name="password" class="form-control" id="password"
                                     autocomplete="off" required>
